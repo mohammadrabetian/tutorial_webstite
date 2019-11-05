6e0776ac0b6214b49ecc4712700912ced9702a74
mohammadrabetian <tommyrabetian@gmail.com>
2019-11-05 21:31:16 +0330

    added a custom admin view and added tinymce app to the project

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.db import models
+from .models import Tutorial
+from tinymce.widgets import TinyMCE
 
-# Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
+        ("Content", {"fields": ["tutorial_content"]})
+    ]
+
+    formfield_overrides = {
+        models.TextField: {"widget": TinyMCE()},
+    }
+admin.site.register(Tutorial, TutorialAdmin)
