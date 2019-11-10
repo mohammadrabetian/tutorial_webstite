6d703bea5b88cc319e273fec1ea3e02b4a22c2dd
mohammadrabetian <tommyrabetian@gmail.com>
2019-11-10 20:54:13 +0330

    added user registraion form and template

--- main/api/urls.py
+++ main/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views.homepage import homepage
+from .views.register import register
 
 
 urlpatterns = [
-    path('home/', homepage, name='homepage')
+    path('home/', homepage, name='homepage'),
+    path('register/', register, name='register'),
 ]
---
+++ main/api/views/register.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect("main:homepage")
+
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+            return render(request = request,
+                          template_name = "main/register.html",
+                          context={"form":form})
+
+    form = UserCreationForm
+    return render(request = request,
+                  template_name = "main/register.html",
+                  context={"form":form})
